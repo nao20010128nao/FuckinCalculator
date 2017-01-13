6881bfe195f951075f6c2a60b0c9042bfe14a536
nao20010128nao <nao20010128@gmail.com>
2017-01-14 00:51:52 +0900

    計算メソッド

--- app/src/main/groovy/com/nao20010128nao/FuckinCalculator/Utils.groovy
+++ app/src/main/groovy/com/nao20010128nao/FuckinCalculator/Utils.groovy
@@ -1,7 +1,59 @@
 package com.nao20010128nao.FuckinCalculator
 
-/**
- * Created by nao on 2017/01/14.
- */
-class Utils {
+public class Utils {
+    /**
+     * Calculates nPr
+     * */
+    public static BigInteger nPr(BigInteger n,BigInteger r){
+        if(n==r)return factorial(n)//nPn=n!
+        if(r==BigInteger.ONE)return n//nP1=n
+        if(r==BigInteger.ZERO)return BigInteger.ZERO//nP0=0
+        BigInteger now=BigDecimal.ONE
+        r.times {
+            now*=n
+            n--
+        }
+        return now
+    }
+    /**
+     * Calculates n!
+     * */
+    public static BigInteger factorial(BigInteger n){
+        if(n==BigInteger.ONE)return BigInteger.ONE//1!=1
+        BigInteger now=BigInteger.ONE
+        n.times {
+            now*=(it+1)
+        }
+        return now
+    }
+    /**
+     * Calculates nCr<br>
+     * Equals to nPr(n,r)/factorial(r)
+     * */
+    public static BigInteger nCr(BigInteger n,BigInteger r){
+        return nPr(n,r)/factorial(r)
+    }
+    /**
+     * Calculates coefficients of x and y for multiplying (x+y)^n style formula
+     * */
+    public static BigInteger[][] multiplyCoefficients(BigInteger factor,BigInteger xFactor=1,BigInteger yFactor=1){
+        def pow={BigInteger a,BigInteger b->
+            if(a==BigInteger.ONE)return BigInteger.ONE//1^n=1
+            if(b==BigInteger.ZERO)return BigInteger.ONE//n^0=1
+            if(b==BigInteger.ONE)return a//n^1=a
+            BigInteger now=BigInteger.ONE
+            b.times{
+                now*=a
+            }
+            return now
+        }
+        BigInteger[][] values=[]
+        BigInteger.ZERO.upto(factor) {
+            BigInteger xMultiply=factor-it
+            BigInteger yMultiply=it
+            BigInteger multiplied=nCr(factor,it as BigInteger)*pow(xFactor,xMultiply)*pow(yFactor,yMultiply)
+            values+=[/*r*/it,multiplied]
+        }
+        return values
+    }
 }
