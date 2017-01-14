e80377df5f52522e7368167f65fa7f0d5cf67003
nao20010128nao <nao20010128@gmail.com>
2017-01-14 13:42:08 +0900

    あ

--- app/src/main/groovy/com/nao20010128nao/FuckinCalculator/TheApplication.kt
+++ app/src/main/groovy/com/nao20010128nao/FuckinCalculator/TheApplication.kt
@@ -1,6 +1,7 @@
 package com.nao20010128nao.FuckinCalculator
 
 import android.app.Application
+import android.support.multidex.MultiDex
 import com.beardedhen.androidbootstrap.TypefaceProvider
 
 class TheApplication : Application() {
