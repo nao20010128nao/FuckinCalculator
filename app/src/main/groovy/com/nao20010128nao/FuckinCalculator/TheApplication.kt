b6a52d5687661501dd6e90f64ef989aa2e1c6c79
nao20010128nao <nao20010128@gmail.com>
2017-01-14 12:58:24 +0900

    追加

---
+++ app/src/main/groovy/com/nao20010128nao/FuckinCalculator/TheApplication.kt
@@ -0,0 +1,9 @@
+package com.nao20010128nao.FuckinCalculator
+
+import android.app.Application
+
+class TheApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
